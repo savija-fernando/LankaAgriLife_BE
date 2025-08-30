892588464c90b1619589f82ed41073dab907c3d5
Savija Fernando <savijanethika@gmail.com>
2025-08-30 21:42:35 +0530

    inventorySchema

---
+++ models/inventory.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose');
+
+const inventorySchema = new mongoose.Schema({
+    inventory_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    itemName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    },
+    expiryDate: {
+        type: Date
+    },
+    stockLevel: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    unitPrice: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+}, {
+    timestamps: true   // adds createdAt & updatedAt automatically
+});
+
+// Create model
+const Inventory = mongoose.model("Inventory", inventorySchema);
+
+module.exports = Inventory;
+
