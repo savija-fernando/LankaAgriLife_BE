393ef2c6965094101acc6c699b60738e36eb7815
Savija Fernando <savijanethika@gmail.com>
2025-09-01 23:39:15 +0530

    Update

--- routes/Inventory.js
+++ routes/Inventory.js
@@ -1,6 +1,7 @@
 const router=require("express").Router();
 const { error } = require("console");
 let Inventory=require("../models/inventory");
+const { Router } = require("express");
 
 router.route("/add").post((req,res)=>{ //when inserting we use post
     //getting the the values using request
@@ -19,7 +20,7 @@ router.route("/add").post((req,res)=>{ //when inserting we use post
         expiryDate,
         stockLevel,
         unitPrice
-    })
+    });
     //saveing to Database
     newInventory.save().then(()=>{
         res.json("Inventory item added!");
@@ -27,11 +28,58 @@ router.route("/add").post((req,res)=>{ //when inserting we use post
     .catch((error)=>{
         console.log(error);
         res.status(500).json("Error:"+error);
-    })
+    });
+});
+//http//localhost:8070/Inventory
+    //get all data
+    router.route("/").get((req,res)=>{
+        Inventory.find().then((inventories)=>{
+            res.json(inventories)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    });
+//http//localhsot:8070/Inventory/update/
+    router.route("/update/:id").put(async (req, res) => {
+    // Extract inventory_id from the URL
+    let inventoryId = req.params.id;
+
+    // Destructure request 
+    const { inventory_id, itemName, dateAdded, expiryDate, stockLevel, unitPrice } = req.body;
+
+    // Build update object
+    const updateInventory = {
+        inventory_id,
+        itemName,
+        dateAdded,
+        expiryDate,
+        stockLevel,
+        unitPrice
+    };
+
+    try {
+        // Use custom field in the query
+        const update = await Inventory.findOneAndUpdate(
+            { inventory_id: inventoryId }, 
+            updateInventory,
+            { new: true }
+        );
+            if (!update) {
+            return res.status(404).json({ message: "Inventory item not found" });
+            }
+
+            res.status(200).json({ message: "Inventory updated successfully", data: update });
+
+        } catch (error) {
+             res.status(500).json({ message: "Error updating inventory", error: error.message });
+        }
+
+    }); 
 
     module.exports=router;  //its a must
 
 
 
+
+
     
-})  
