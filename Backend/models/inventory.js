892cd70806f85fa0c15c90d321b58f3e879d9633
Savija Fernando <savijanethika@gmail.com>
2025-09-05 19:20:41 +0530

    redo

---
+++ Backend/models/inventory.js
@@ -0,0 +1,45 @@
+const mongoose=require('mongoose');
+
+const inventorySchema = new mongoose.Schema({
+    inventory_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    itemName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    dateAdded: {
+        type: Date,
+        
+    },
+    expiryDate: {
+        type: Date
+    },
+    stockLevel: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    unitPrice: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    threshold:{   //low stock alert
+        type:Number,
+        default:10,
+        min:0
+
+    }
+}, {
+    timestamps: true   // adds createdAt & updatedAt automatically
+});
+
+// Create model
+const Inventory = mongoose.model("Inventory", inventorySchema);
+
+module.exports = Inventory;
+
---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");    // assigning variables
+
+dotenv.config();  // load .env file
+
+const app = express();
+
+const PORT = process.env.PORT || 8070;   //if 8070 port is not there assign the available port number(process.env.PORT)
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;  // access the database
+
+// mongoose connection 
+mongoose.connect(URL)
+    .then(() => console.log("MongoDB Connection success!"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+
+const InventoryRouter=require("./routes/Inventory");
+app.use("/Inventory",InventoryRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
